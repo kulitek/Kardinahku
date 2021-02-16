8ae03781006d9736e8e66c09cfd0faf03c23f6f0
Oskop <oskop17@gmail.com>
2021-02-16 20:33:03 +0700

    penambahan model entitas lain

--- models.py
+++ models.py
@@ -45,10 +45,49 @@ class KategoriMasalah(Base):
     id = Column(Integer, primary_key=True, index=True)
     kategori = Column(String(60))
 
+    masalah = relationship("Masalah", back_populates="kategori_masalah")
+
+class Instalasi(Base):
+    __tablename__ = "instalasi"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nama = Column(String(40))
+
+    ruangan = relationship("Ruangan", back_populates="instalasi")
+
+
 class Ruangan(Base):
     __tablename__ = "ruangan"
 
     id = Column(Integer, primary_key=True, index=True)
+    nama = Column(String(60))
+    id_instalasi = Column(Integer, ForeignKey("instalasi.id"))
+
+    instalasi = relationship("Instalasi", back_populates="ruangan")
+    masalah = relationship("Masalah", back_populates="ruangan")
+
+class JenisSarana(Base):
+    __tablename__ = "jenis_sarana"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nama = Column(String(30))
+
+    sarana = relationship("Sarana", back_populates="jenis")
+
+
+class Sarana(Base):
+    __tablename__ = "sarana"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nama = Column(String(90))
+    berat = Column(String(30), nullable=True)
+    panjang = Column(String(30), nullable=True)
+    lebar = Column(String(30), nullable=True)
+    tinggi = Column(String(30), nullable=True)
+
+    id_jenis = Column(Integer, ForeignKey("jenis_sarana.id"))
+    # id_kamar = Column(Integer, ForeignKey(""))
+
 
 
 class Masalah(Base):
@@ -58,3 +97,13 @@ class Masalah(Base):
     deskripsi = Column(Text)
     id_kategori_masalah = Column(Integer, ForeignKey("kategori_masalah.id"))
     id_ruangan = Column(Integer, ForeignKey("ruangan.id"))
+    id_sarana = Column(Integer, ForeignKey("sarana.id"))
+    selesai = Column(Boolean, default=False)
+
+    created_at = Column(DateTime, default=datetime.now)
+    update_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    done_at = Column(DateTime, nullable=True)
+    deleted_at = Column(DateTime, nullable=True)
+
+    ruangan = relationship("Ruangan", back_populates="masalah")
+    kategori_masalah = relationship("KategoriMasalah", back_populates="masalah")
