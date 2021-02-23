a8b69ccd0f697469850b4e5be5273805aa7b2f3a
Oskop <oskop17@gmail.com>
2021-02-23 14:10:03 +0700

    ini

---
+++ schemas/user_scheme.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    _repr_hide = ['created_at', 'updated_at', 'deleted_at']
+    username: str
+
+class UserRegister(UserBase):
+    password: str
+    email: str
+    id_pegawai: int
+    role: Optional[str]
+
+class UserLogin(UserBase):
+    password: str
+
+class UserUpdate(UserBase):
+    id: int
+    email: Optional[str]
+    role: Optional[str]
+    is_active: Optional[bool]
+
+class User(UserBase):
+    id: int
+    email: Optional[str]
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str = None
